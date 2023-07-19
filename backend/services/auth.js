81aec523a0083487a9a2a2d99d761969e8f7a04a
Mohi <56826704+MohiMad@users.noreply.github.com>
2023-07-19 19:59:39 +0200

    Update auth.js

--- backend/services/auth.js
+++ backend/services/auth.js
@@ -6,6 +6,7 @@ const axios = require("axios");
 const url = require("url");
 
 function createOAuth2SearchParams(code) {
+    console.log("domain?", domain);
     return new URLSearchParams({
         client_id: clientId,
         client_secret: clientSecret,
@@ -26,6 +27,8 @@ async function getOAuthData(code) {
             'Content-Type': 'application/x-www-form-urlencoded',
         }
     });
+
+    console.log("tokenResponseData", tokenResponseData);
 
     return tokenResponseData.body.json();
 }
